d16f42b0445190694541c81fa829dc44a81a75f2
GuilhermeBobsin <gbobsin3@gmail.com>
2025-06-25 00:14:40 -0300

    Comentar rota de login no arquivo web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('welcome');
-});
+// Route::get('/login', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
