0383413f1c480ee6f05e46b8345c7788aa239cdc
GuilhermeBobsin <gbobsin3@gmail.com>
2025-06-25 00:12:40 -0300

    Atualizar link de login na view welcome.blade.php e incluir autoload no arquivo auth.php

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
